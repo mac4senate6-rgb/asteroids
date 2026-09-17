c33c23f6809c7897b932fc770cd74435e845c87e
Angus McKelvey <LahainaCyberSystems@gmail.com>
2026-09-16 18:34:14 -1000

    I: Add shot world wrapping

--- shot.py
+++ shot.py
@@ -11,8 +11,22 @@ class Shot(CircleShape):
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(surface=screen, color=(255, 255, 255), center=self.position, radius=self.radius, width=LINE_WIDTH)
 
+    def wrap_screen(self) -> None:
+        if self.position.x < -self.radius:
+            self.position.x = SCREEN_WIDTH + self.radius
+        elif self.position.x > SCREEN_WIDTH + self.radius:
+            self.position.x = -self.radius
+
+        if self.position.y < -self.radius:
+            self.position.y = SCREEN_HEIGHT + self.radius
+        elif self.position.y > SCREEN_HEIGHT + self.radius:
+            self.position.y = -self.radius
+
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+        self.wrap_screen()
+
         self.lifetime -= dt
+
         if self.lifetime <= 0:
             self.kill()
