7c009dad0b21f114f813f1248ba394e00eaa9e65
Angus McKelvey <LahainaCyberSystems@gmail.com>
2026-09-16 18:01:33 -1000

    H: Add finite shot lifetime

--- shot.py
+++ shot.py
@@ -6,9 +6,13 @@ from constants import *
 class Shot(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, SHOT_RADIUS)
+        self.lifetime = SHOT_LIFETIME_SECONDS
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(surface=screen, color=(255, 255, 255), center=self.position, radius=self.radius, width=LINE_WIDTH)
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+        self.lifetime -= dt
+        if self.lifetime <= 0:
+            self.kill()
